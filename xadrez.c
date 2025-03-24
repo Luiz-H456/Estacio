1fef7904a6fce903c4bda1c3458ea78d5cff4769
Luiz-H456 <luuiizhcc@gmail.com>
2025-03-24 11:46:24 -0300

    xadrez 1.0

--- xadrez.c
+++ xadrez.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+int main(){
+    int torre, bispo, rainha;
+    for(torre = 1; torre <= 5; torre++){
+        printf("Torre moveu-se uma casa para a ");
+        printf("Direita\n");
+    }
+      for(bispo = 1; bispo <= 5; bispo++){
+        printf("bispo moveu-se uma casa para ");
+        printf("Cima Esquerda\n");
+    }
+       for(rainha = 1; rainha <= 8; rainha++){
+        printf("rainha moveu-se uma casa para a ");
+        printf("Esquerda\n");
+    }
+    return 0;
+}
